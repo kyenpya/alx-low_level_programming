bdfe8c45a315f66b8ff8d4c534ee95b30b97c3ba
kyenpya <kyenpyagutap@gmail.com>
2023-08-10 08:24:42 -0700

    size, inteland quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - printing using the fputs()
+ * Description - fputs() also used to print output
+ * Return: 1 (means success)
+ */
+
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
