ac6a78200a88ed4518c2048ca76d3cb66c3a9d15
kyenpya <kyenpyagutap@gmail.com>
2023-08-10 07:13:09 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - using the printf() on here
+ * Description - print the text and end with a new line
+ * Return: 0 (means success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
