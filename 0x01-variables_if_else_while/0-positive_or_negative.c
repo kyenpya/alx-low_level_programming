fb56c2b16a8303591e99184c1fde251ac4d872ec
kyenpya <kyenpyagutap@gmail.com>
2023-08-11 06:05:34 -0700

    Positive or negative random numbers

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description - Prints random numbers and assign them to the variable n
+ * Return: 0 (always success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+
+	printf("%i is ", n);
+
+	if (n > 0)
+	{
+		printf("positive\n");
+	}
+	else if (n < 0)
+	{
+		printf("negative\n");
+	}
+	else
+	{
+		printf("zero\n");
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+C programming in depth 1
