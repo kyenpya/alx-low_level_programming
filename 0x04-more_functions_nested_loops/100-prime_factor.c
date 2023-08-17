e4ddf957b65b3f0d9d094b3c36b55857b63d8c70
kyenpya <kyenpyagutap@gmail.com>
2023-08-17 13:47:54 -0700

    prime numbers and numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Entry point
+ * Description - prints largest prime factor 612852475143
+ * then a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int i, n = 612852475143;
+
+	for (i = 3; i < 782849; i = i + 2)
+	{
+		while ((n % i == 0) && (n != i))
+			n = n / i;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,2 +1,23 @@
 #include "main.h"
 
+/**
+ * print_number - prints integer
+ * @n: integer parameter
+ *Return: 0 (success)
+ */
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
