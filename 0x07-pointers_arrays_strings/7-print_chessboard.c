8bb31615ad41f6ced815491e67a95b1a73e135d3
kyenpya <kyenpyagutap@gmail.com>
2023-08-28 10:46:50 -0700

    print_chessboard and print_diagsums

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_chessboard - prints a chessboard
+  * @a: a 2D array representing the chessboard
+  */
+
+void print_chessboard(char (*a)[8])
+{
+	int rows, cols;
+
+	for (rows = 0; rows < 8; rows++)
+	{
+		for (cols = 0; cols < 8; cols++)
+		{
+			_putchar(a[rows][cols]);
+		}
+		_putchar('\n');
+	}
+}
