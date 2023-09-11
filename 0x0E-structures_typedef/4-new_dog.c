37098748e421dd100039fda72220a7f61b57b837
kyenpya <kyenpyagutap@gmail.com>
2023-09-11 12:13:23 -0700

    Tasks 2 to 5

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include "dog.h"
+
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * strlen - gets string length
+ * @str: string to be measured
+ * Return: length of the string
+ */
+int _strlen(char *str)
+{
+	int l = 0;
+
+	while (*str++)
+		l++;
+	return (l);
+}
+
+/**
+ * _strcopy - copies string from src including \0, to dest
+ * @dest: destination for copied string
+ * @src: source string
+ * Return: ptr to dest
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: struct dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	dog->name = _strcopy(dog->name, name);
+	dog->age = age;
+	dog->owner = _strcopy(dog->owner, owner);
+
+	return (dog);
+}
