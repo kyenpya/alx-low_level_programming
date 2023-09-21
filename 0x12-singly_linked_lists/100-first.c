6655bf7b411d581f4ba3cbef8ce865a6d5bfeaa5
kyenpya <kyenpyagutap@gmail.com>
2023-09-21 07:15:39 -0700

    Task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void before_main(void);
+/**
+ * before_main - prints amessage before the main function is executed
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void before_main(void);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	before_main();
+
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
+}
