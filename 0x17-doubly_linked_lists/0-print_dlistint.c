7e67979b0219af097649d911576558fe06e2dd09
kyenpya <kyenpyagutap@gmail.com>
2024-03-21 09:16:50 -0700

    task:files 4 and five

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * print_dlistint - print all elements of a doubly linked list
- * @h: the head node
+ * print_dlistint - prints all the elements of a doubly linked list
+ * @h: head node
  *
- * Return:number of nodes
+ * Return: number of nodes
  */
 
 size_t print_dlistint(const dlistint_t *h)
